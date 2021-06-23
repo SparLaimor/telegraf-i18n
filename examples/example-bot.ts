16576f04d4cdb36ec3d87a12da8025c9450cb04f
SparLaimor <sparlaimor@gmail.com>
2021-06-23 09:59:41 +0300

    0.2.3

--- examples/example-bot.ts
+++ examples/example-bot.ts
@@ -26,7 +26,6 @@ const i18n = new I18n({
   }
 })
 
-// eslint-disable-next-line @typescript-eslint/dot-notation
 const bot = new Telegraf<MyContext>(process.env['BOT_TOKEN']!)
 bot.use(session())
 bot.use(i18n.middleware())
